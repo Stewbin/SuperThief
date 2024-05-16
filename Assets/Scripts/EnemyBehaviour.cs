8f0688f76d0164eff1763b339d36eb85e3367a03
Stewbin <pkmn8265@gmail.com>
2024-05-15 21:22:56 -0400

    Non-FSM Enemy Behavior finished (in theory...)

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Palmmedia.ReportGenerator.Core.Parser.Analysis;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UIElements;
 
 public class EnemyBehaviour : MonoBehaviour
 {
@@ -14,39 +16,41 @@ public class EnemyBehaviour : MonoBehaviour
     public float MoveSpeed = 1f;
     public float StoppingDistance = 0.5f;
     public float WanderDistance = 1f;
-    States state = States.Wander;
+    States CurrentState = States.Wander;
     private NavMeshAgent Agent;
     Vector3 HomePoint;
+    public Transform Player;
 
     
     // Start is called before the first frame update
     void Start()
     {
         Agent = GetComponent<NavMeshAgent>();
-        HomePoint = transform.position;        
+        HomePoint = transform.position;      
     }
 
     // Update is called once per frame
     void Update()
     {
         Agent.stoppingDistance = StoppingDistance;
-        Agent.speed = MoveSpeed;
-        
-        if(state == States.Wander)
+        Agent.speed = MoveSpeed;       
+        // Update CurrentState
+        CurrentState = SensePlayer() ? States.Chase : States.Wander;
+
+        if(CurrentState == States.Wander)
         {
             if(Agent.pathStatus == NavMeshPathStatus.PathComplete)
             {
                 Agent.SetDestination(RandomPoint(HomePoint));
             }
+        } 
+        if(CurrentState == States.Chase)
+        {
+            Agent.SetDestination(Player.position);
         }
-
-    }
-    void LateUpdate()
-    {
-        DrawVisionCone();
     }
 
-    void DrawVisionCone()
+    bool SensePlayer()
     {
         // Convert degrees to radians
         float _xFOV = xFOV * (Mathf.PI / 180);
@@ -61,10 +65,13 @@ public class EnemyBehaviour : MonoBehaviour
                 float x = Mathf.Cos(phi) * Mathf.Sin(theta);
                 float z = Mathf.Cos(phi) * Mathf.Cos(theta);
 
-                Vector3 Ray = new Vector3(x, y, z) * Radius;
-                Debug.DrawRay(transform.position, Ray, Color.red);
+                Vector3 Ray = new Vector3(x, y, z);
+                RaycastHit hit;
+                if(Physics.Raycast(transform.position, Ray, out hit, Radius))
+                    return hit.transform.CompareTag("Player");
             }
         }
+        return false;
     }
 
     Vector3 RandomPoint(Vector3 centerPoint)
