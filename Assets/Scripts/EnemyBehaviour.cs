f7217d184564044d68c0fdb16004e3c48eed3ab9
Stewbin <pkmn8265@gmail.com>
2024-05-15 20:06:30 -0400

    Nav mesh won't work :(

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -9,25 +9,41 @@ public class EnemyBehaviour : MonoBehaviour
     public float xFOV = 60f;
     public float yFOV = 60f;
     [SerializeField] float Radius = 5f;
-    [SerializeField] float TurnFraction = 2f;
+    [SerializeField] float TurnFraction = 0.5f;
     [Header("Movement Parameters")]
     public float MoveSpeed = 1f;
     public float StoppingDistance = 0.5f;
+    public float WanderDistance = 1f;
+    States state = States.Wander;
+    private NavMeshAgent Agent;
+    Vector3 HomePoint;
 
     
     // Start is called before the first frame update
     void Start()
     {
-        var NavMeshAgent = GetComponent<NavMeshAgent>();
-        NavMeshAgent.stoppingDistance = StoppingDistance;
-        NavMeshAgent.speed = MoveSpeed;
+        Agent = GetComponent<NavMeshAgent>();
+        HomePoint = transform.position;        
     }
 
     // Update is called once per frame
+    void Update()
+    {
+        Agent.stoppingDistance = StoppingDistance;
+        Agent.speed = MoveSpeed;
+        
+        if(state == States.Wander)
+        {
+            if(Agent.pathStatus == NavMeshPathStatus.PathComplete)
+            {
+                Agent.SetDestination(RandomPoint(HomePoint));
+            }
+        }
+
+    }
     void LateUpdate()
     {
         DrawVisionCone();
-        //Debug.DrawRay(transform.position, new Vector3(Mathf.Cos(Mathf.PI / 3),0,Mathf.Sin(Mathf.PI / 3)) * Radius, Color.green);
     }
 
     void DrawVisionCone()
@@ -36,9 +52,9 @@ public class EnemyBehaviour : MonoBehaviour
         float _xFOV = xFOV * (Mathf.PI / 180);
         float _yFOV = yFOV * (Mathf.PI / 180); 
         
-        for(float theta = -_xFOV; theta < xFOV; theta += 1)//xFOV / TurnFraction )
+        for(float theta = -_xFOV; theta < _xFOV; theta += _xFOV / TurnFraction )
         {
-            for(float phi = -_yFOV; phi < yFOV; phi += 1)//yFOV / TurnFraction)
+            for(float phi = -_yFOV; phi < _yFOV; phi += _yFOV / TurnFraction)
             {
                 // Convert spherical to cartesian coordinates
                 float y = Mathf.Sin(phi);
@@ -50,6 +66,20 @@ public class EnemyBehaviour : MonoBehaviour
             }
         }
     }
+
+    Vector3 RandomPoint(Vector3 centerPoint)
+    {
+        Vector3 randomPoint = centerPoint + Random.insideUnitSphere * WanderDistance; //random point in a sphere
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas)) 
+            return hit.position;
+        else 
+            return Vector3.zero;
+    }
 }
 
-
+enum States
+{
+    Wander,
+    Chase
+}
