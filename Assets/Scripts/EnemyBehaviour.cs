ef7733a8afc6e3386da1576a5289f40e7e719f34
Stewbin <pkmn8265@gmail.com>
2024-05-17 15:59:29 -0400

    Fixed more bugs with the AI

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -8,11 +9,11 @@ public class EnemyBehaviour : MonoBehaviour
     public float xFOV = 60f;
     public float yFOV = 60f;
     [SerializeField] float Radius = 5f;
-    [SerializeField] float TurnFraction = 0.5f;
+    [SerializeField] float RayDensity = 0.5f;
     [Header("Movement Parameters")]
     public float MoveSpeed = 1f;
     public float StoppingDistance = 0.5f;
-    public float WanderDistance = 1f;
+    public float WanderDistance = 3f;
     [SerializeField] States CurrentState = States.Wander;
     private NavMeshAgent Agent;
     Vector3 HomePoint;
@@ -36,12 +37,12 @@ public class EnemyBehaviour : MonoBehaviour
         
         if(CurrentState == States.Wander)
         {
-            if(!Agent.pathPending)
+            if(Agent.remainingDistance <= Agent.stoppingDistance)
             {
-                Vector3 rando = RandomPoint(HomePoint);
-                Debug.Log($"Random point: {rando}");
-                Agent.SetDestination(rando);
+                Agent.SetDestination(RandomPoint(HomePoint));
+                transform.LookAt(Agent.velocity);
             }
+            // Debug.Log($"Random point: {Agent.destination}");
         } 
         else if(CurrentState == States.Chase)
         {
@@ -59,27 +60,36 @@ public class EnemyBehaviour : MonoBehaviour
         float _xFOV = xFOV * (Mathf.PI / 180);
         float _yFOV = yFOV * (Mathf.PI / 180); 
         
-        for(float theta = -_xFOV; theta < _xFOV; theta += _xFOV / TurnFraction )
+        for(float theta = -_xFOV; theta < _xFOV; theta += _xFOV / RayDensity )
         {
-            for(float phi = -_yFOV; phi < _yFOV; phi += _yFOV / TurnFraction)
+            for(float phi = -_yFOV; phi < _yFOV; phi += _yFOV / RayDensity)
             {
                 // Convert spherical to cartesian coordinates
                 float y = Mathf.Sin(phi);
                 float x = Mathf.Cos(phi) * Mathf.Sin(theta);
                 float z = Mathf.Cos(phi) * Mathf.Cos(theta);
 
-                Vector3 Ray = new Vector3(x, y, z);
+                // Package it as one vector, and convert it to world coordinates
+                Vector3 Ray = transform.TransformDirection(new Vector3(x, y, z));
+                Debug.DrawRay(transform.position, Ray * Radius, Color.red);
+                
                 RaycastHit hit;
-                if(Physics.Raycast(transform.position, Ray, out hit, Radius))
+                if(Physics.Raycast(transform.position, Ray, out hit, Radius, LayerMask.NameToLayer("Water")))
                     return hit.transform.CompareTag("Player");
+                
             }
         }
         return false;
     }
 
+    /// <summary>
+    /// Returns a random point on the navmesh within a sphere of radius WanderDistance
+    /// </summary>
+    /// <param name="centerPoint"></param>
+    /// <returns></returns>
     Vector3 RandomPoint(Vector3 centerPoint)
     {
-        Vector3 randomPoint = centerPoint + Random.insideUnitSphere * WanderDistance; // Random point in a sphere
+        Vector3 randomPoint = centerPoint + (Random.insideUnitSphere * WanderDistance); // Random point in a sphere
         
         NavMeshHit hit;
         // Find closest point to randomPoint by projecting onto NavMesh 
