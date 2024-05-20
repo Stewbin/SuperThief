606cadcdce5dbdac94eee2a6cf71732570ba8668
Stewbin <pkmn8265@gmail.com>
2024-05-20 18:31:07 -0400

    Y vscode forget my classes?

--- Assets/Scriptable Objects/Inventory System/InventoryBehaviour.cs
+++ Assets/Scriptable Objects/Inventory System/InventoryBehaviour.cs
@@ -1,15 +1,25 @@
 using UnityEngine;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 
 public class InventoryBehaviour : MonoBehaviour {
-    Dictionary<ItemObject, int> Inventory = new(5);
+    [SerializeField] private List<ItemStack> Inventory = new();
 
     // Picking up items
     private void OnTriggerEnter(Collider other) {
         // Add item to inventory 
-        if(TryGetComponent(out ItemBehaviour item)) {
-            Inventory.Add(item.itemObject, item.Amount);
+        if(TryGetComponent(out ItemBehaviour itemStack)) {
+            
             Destroy(other.gameObject);
         }
+    }
+
+    public class ItemStack
+    {
+        public ItemDetails Item;
+        public int amount {get; private set;} = 1;
+
+        public void AddAmount(int amount) {this.amount += amount;}
+
     }
 }
