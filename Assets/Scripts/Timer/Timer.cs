1247d3f90f39396a35edf19a85ec26ea167e0eb3
christianmonfiston <109248449+christianmonfiston@users.noreply.github.com>
2024-05-20 22:50:16 -0400

    added notifications, timer, input system

---
+++ Assets/Scripts/Timer/Timer.cs
@@ -0,0 +1,48 @@
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; 
+
+public class Timer : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] public TextMeshProUGUI timerText;
+
+    [SerializeField] public float elapsedTime; 
+    
+    [SerializeField] public float remainingTime;
+  void  Update()
+  {
+    CountDown();
+  }
+
+  public void ElapsedTime(){
+elapsedTime += Time.deltaTime; 
+    int minutes = Mathf.FloorToInt(elapsedTime/ 60);
+    int seconds = Mathf.FloorToInt(elapsedTime % 60);
+
+    timerText.text = string.Format("{0:00} : {1:00}", minutes , seconds); 
+  }
+
+   public void CountDown(){
+
+    if (remainingTime > 0){
+remainingTime -= Time.deltaTime; 
+
+    } else if (remainingTime < 0){
+        remainingTime = 0;
+
+        
+    }
+
+    int minutes = Mathf.FloorToInt(remainingTime/ 60);
+    int seconds = Mathf.FloorToInt(remainingTime % 60);
+
+    timerText.text = string.Format("{0:00} : {1:00}", minutes , seconds); 
+  }
+
+  
+}
