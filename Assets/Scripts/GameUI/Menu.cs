d3260ef57a550964919f618939888446f5b953f7
christianmonfiston <christianoliviermonfiston@gmail.com>
2024-05-22 18:07:45 -0400

    lot of cha nges, notif implemented, interaction system almost done

--- Assets/Scripts/GameUI/Menu.cs
+++ Assets/Scripts/GameUI/Menu.cs
@@ -5,25 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    [SerializeField] private AndroidNotificationHandler androidNotificationHandler;
-    [SerializeField] private IOSNotificationHandler iosNotificationHandler;
-
-
+  
     public void Play(){
         //CLick Play to start playing
         SceneManager.LoadScene(1);
         print("Successfully started playing");
     }
-
     
-    void SendNotificationAndroid()
-    {
-
-#if UNITY_ANDROID
-        androidNotificationHandler.ScheduleNotification(Time.deltaTime);
-
-#elif UNITY_IOS
-        iosNotificationHandler.ScheduleNotification(1);
-#endif
-    }
 }
