b99f6bd2cdb660299fdb333c7f286fc95b79c1c4
christianmonfiston <109248449+christianmonfiston@users.noreply.github.com>
2024-07-19 16:34:47 -0400

    testing stuff

--- Assets/Scripts/Networking/Leaderboard.cs
+++ Assets/Scripts/Networking/Leaderboard.cs
@@ -6,14 +6,15 @@ using TMPro;
 public class Leaderboard : MonoBehaviour
 {
 
-    public TMP_Text playerNameText, killsText, deathsText, moneyText; 
+    public TMP_Text playerNameText, killsText, deathsText, moneyText, rankText; 
 
-    public void SetDetails(string name, int kills, int deaths, int money){
+    public void SetDetails(string name, int kills, int deaths, int money,int rank){
 
         playerNameText.text = name; 
         killsText.text = kills.ToString(); 
         deathsText.text = deaths.ToString(); 
         moneyText.text = money.ToString();
+        rankText.text = rank.ToString();
         
 
 
